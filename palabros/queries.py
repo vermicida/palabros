c7d40f016a1445d8bde857d86ebf8644e8edfbfe
Diego Herrera <vermicida@gmail.com>
2022-02-06 20:29:05 +0100

    Rebuild the dictionary from the scratch

--- palabros/queries.py
+++ palabros/queries.py
@@ -6,7 +6,7 @@ class Query:
 
     WordExists = """
         SELECT
-            COUNT(w.word) AS total
+            COUNT(*) AS total
         FROM
             words AS w
         WHERE
@@ -16,12 +16,20 @@ class Query:
     GetRandomWord = """
         SELECT
             w.word
-        FROM
-            words AS w
-            LEFT OUTER JOIN
-                games AS g ON w.word = g.word
-        WHERE
-            g.id IS NULL
+        FROM (
+            SELECT
+                w.word
+            FROM
+                words AS w
+                LEFT OUTER JOIN
+                    games AS g ON w.word = g.word
+            WHERE
+                g.id IS NULL
+            ORDER BY
+                w.frequency DESC
+            LIMIT
+                50
+        ) as w
         ORDER BY
             RANDOM()
         LIMIT
@@ -72,6 +80,68 @@ class Query:
         VALUES (
             ?,
             ?,
+            ?
+        );
+    """
+
+    TableExists = """
+        SELECT
+            COUNT(*) AS total
+        FROM
+            sqlite_master
+        WHERE
+            type = 'table'
+        AND
+            name = ?;
+    """
+
+    CreateVersionsTable = """
+        CREATE TABLE IF NOT EXISTS versions (
+            version TEXT PRIMARY KEY
+        );
+    """
+
+    CreateWordsTable = """
+        CREATE TABLE IF NOT EXISTS words (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT NOT NULL UNIQUE,
+            frequency REAL NOT NULL
+        );
+    """
+
+    CreateGamesTable = """
+        CREATE TABLE IF NOT EXISTS games (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT NOT NULL,
+            date TEXT NOT NULL,
+            FOREIGN KEY (word) REFERENCES words (word)
+        );
+    """
+
+    CreateAttemptsTable = """
+        CREATE TABLE IF NOT EXISTS attempts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            game_id INTEGER NOT NULL,
+            word TEXT NOT NULL,
+            match INTEGER NOT NULL,
+            FOREIGN KEY (game_id) REFERENCES games (id)
+        );
+    """
+
+    CreateGamesDateIndex = """
+        CREATE UNIQUE INDEX IF NOT EXISTS idx_games_date ON games (date);
+    """
+
+    PopulateWordsTable = """
+        INSERT OR IGNORE INTO words (word, frequency) VALUES {};
+    """
+
+    CreateVersion = """
+        INSERT INTO
+            versions (
+                version
+            )
+        VALUES (
             ?
         );
     """
