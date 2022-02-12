d5dd45d8d0b99d431192ea9dc1e8c111ce8c0fde
Diego Herrera <vermicida@gmail.com>
2022-02-13 00:33:11 +0100

    Add some flow control prints

--- .github/actions/poetry-tests/entrypoint.sh
+++ .github/actions/poetry-tests/entrypoint.sh
@@ -5,14 +5,17 @@ set -e
 poetry install
 
 if [ $1 == 'true' ]; then
+    echo -e '\n-->> Running linting analysis\n'
     poetry run flake8
 fi
 
 if [ $2 == 'true' ]; then
+    echo -e '\n-->> Running unit testing\n'
     poetry run pytest ./tests
 fi
 
 if [ $3 == 'true' ]; then
+    echo -e '\n-->> Running coverage testing\n'
     poetry run coverage run -m pytest ./tests
     poetry run coverage report --fail-under 80 -m
 fi
